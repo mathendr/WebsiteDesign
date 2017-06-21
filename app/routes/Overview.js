291ceed6830dec202dc7478bcb57baba18d6f17c
Azplap <austinpeng97@gmail.com>
2017-06-20 22:54:05 -0500

    Overview
    What I have of overview

---
+++ app/routes/Overview.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/Overview', function (req, res) {
+    var dataFile = req.app.get('appData');
+    var ID = [];
+    var ProjName = [];
+    var AccName = [];
+    dataFile.Data.forEach(function (item) {
+        ID = ID.concat(item.ID);
+        ProjName = ProjName.concat(item.ProgramName);
+        AccName = AccName.concat(item.AccountName);
+    });
+    res.render('Overview', {
+        pageTitle: 'Home',
+        pageID: "home",
+        ID: ID,
+        ProjName: ProjName,
+        AccountName: AccName,
+        Location: ""
+    });
+});
+
+module.exports = router;

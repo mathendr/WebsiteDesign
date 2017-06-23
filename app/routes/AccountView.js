69e6b8e4baa2e6121c116a889e27a17439fcbcfb
mathendr <matthew.hendrick@hpe.com>
2017-06-23 16:29:56 -0500

    New tab and way to view accounts first

--- app/routes/AccountView.js
+++ app/routes/AccountView.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/Account',function(req,res){
+    var dataFile = req.app.get('appData');
+    var item = dataFile.Data;
+    var itemlist = [];
+    for(i = 0; i < item.length; i++)
+    {
+        if(itemlist.indexOf(item[i].AccountName) == -1)
+            itemlist = itemlist.concat(item[i].AccountName);
+    }
+    res.render('AccountOverview', {
+        pageTitle: 'Account Overview',
+        pageID: "Account Overview",
+        ItemList: itemlist,
+        Location: "../",
+        current: "accountlist"
+    });
+});
+
 router.get('/Account/:AccountName', function (req, res) {
     var dataFile = req.app.get('appData');
     var item = dataFile.Data;
@@ -16,7 +34,7 @@ router.get('/Account/:AccountName', function (req, res) {
         pageID: "Account Overview",
         ItemList: itemlist,
         Location: "../",
-        current: "home"
+        current: "accountlist"
     });
 });
 
